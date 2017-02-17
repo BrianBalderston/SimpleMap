3892b6c2b2f9d941bcd8f39869e04b5460772790
BrianBalderston <b_Balderston@fanshaweonline.ca>
2017-02-17 16:07:20 -0500

    newer version

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 (function() {
-	var map, marker;
+	var map = new google.maps.Map(document.querySelector('.map-wrapper')), marker;
 	
-	function initMap() 
+	function initMap(position) 
 	{
-		map = new google.maps.Map(document.querySelector('.map-wrapper'),{
-			center : { lat: 42.983233, lng: -81.250688 },
-			zoom : 14
-		});
+		map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+			map.setZoom(14);
+		
 		marker = new google.maps.Marker({
-			position : { lat: 42.983233, lng: -81.250688 }, 
+			position : { lat: position.coords.latitude, lng: position.coords.longitude }, 
 			map: map, 
 			title: "Hello World!"
-		});
+		});	
 	}
 	
-	initMap();
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(initMap, handleError);
+	} else {
+		console.log('this is an error?');
+		}
+	
+	function handleError(){
+		console.log('something went wrong');
+	}
+	////initMap();
 })();
